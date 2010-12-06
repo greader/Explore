560fb8dc214c8a5e944cad4d7858311141b6d37b
Gilad Reader <giladreader@gmail.com>
2010-12-06 16:08:10 +0200

    Integrated into KeyboardReader details....

--- src/utilities/InputString.java
+++
@@ -1,9 +0,0 @@
-package utilities;
-import java.io.*;
-
-public class InputString extends KeyboardReader{
-	public String InputString(){  //The constructor method returns the keyboard input
-		return readKeyboard(); //no conversion is necessary - already a string!
-	}
-
-}
--- src/utilities/KeyboardReader.java
+++ src/utilities/KeyboardReader.java
@@ -1,40 +1,39 @@
 package utilities;
 
 import java.io.*;
-
 public class KeyboardReader {
-
-	private BufferedReader reader;
-	// creates an instance variable "reader" of type BufferedReader
-
-	public KeyboardReader() {
-		reader = new BufferedReader(new InputStreamReader(System.in));
-		//passes new System input into new InputStreamReaderobject 
-		//and passes into new BufferedReader and
-		//stores as "reader" variable
-	}
+	static private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	// creates a static variable "reader" of type BufferedReader
+	//passes new System input into new InputStreamReaderobject 
+	//and passes into new BufferedReader and
+	//stores as "reader" variable
 
 	public String readKeyboard() {  //takes keyboard input and stores as local variable "line".
 		String line = "";  //new variable called line to hold the keyboard input
 		try {
 			line = reader.readLine(); //an imported ReadLine method readLine
-			                                          //passes the input to the new "reader" method
+			//passes the input to the new "reader" method
 		} catch (IOException e) {  //if error in input then prints the error
 			e.printStackTrace();
 		}
 		return line;  //the final input!!!
 	}
-	 public String getKeyboardInput() {
+	public String getKeyboardInput() {
 		return readKeyboard();
-	  }
-
-	
-public void prompt (String s){  //prints string s
-System.out.println(s);
-}
-public void display (String s){ //prints string s  (?Why does it have to be static?)
-	System.out.println(s);
 	}
-
-
+	public void prompt (String s){  //prints string s
+		System.out.println(s);
+	}
+	public void display (String s){ //prints string s  (?Why does it have to be static?)
+		System.out.println(s);
+	}
+	public double inputDouble() {  //method returns the keyboard input
+		return Double.parseDouble(readKeyboard()); //Converts string to double
+	}
+	public int inputInteger() {  //method returns the keyboard input
+		return Integer.parseInt(readKeyboard());  //converts string to integer
+	}
+	public String inputString() {  //method returns the keyboard input
+		return readKeyboard(); //no conversion is necessary - already a string!
+	}
 }
